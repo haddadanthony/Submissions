dba6b120ebae720ccbf10982d7dc1dfaef0411b6
Anthony <haddad-anthony@live.com>
2020-01-17 22:28:05 +0200

    Creating table

--- Exercises/SQLite Basic/answers.sh
+++ Exercises/SQLite Basic/answers.sh
@@ -5,7 +5,7 @@
   Where students.age > 30
 
 3 Select name from students
-  where students.Gender is "F" and students.age >= 30
+  where students.Gender is "F" and students.age = 30
 
 4 Select points from students
   where students.name = "Alex"
@@ -21,4 +21,28 @@
   set points = students.Points - 10
   where students.name = "Alex";
 
-8
+
+# Creating Table
+# 1
+create table graduates(
+	id integer not null primary key autoincrement,
+	name text not null unique,
+	age integer,
+	gender text,
+	points integer,
+	graduation date
+);
+
+# 2
+insert into graduates(id, name, age, gender, points)
+select * from students
+where students.name = "Layal"
+
+# 3
+update graduates
+set graduation = "08/09/2019"
+where graduates.name = "Layal"
+
+# 4
+delete from graduates
+where name = "Layal";
