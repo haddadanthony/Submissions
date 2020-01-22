d852e910b091da6ca0f95403a8b4e0341504ce3a
Anthony <haddad-anthony@live.com>
2020-01-22 11:53:37 +0200

    step 3

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -2,5 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Ok!"));
-app.listen(port, () => console.log(`Ok`));
+const getDate = () => {
+  let today = new Date();
+  let hours = today.getHours();
+  let minutes = today.getMinutes();
+
+  return `${hours}:${minutes}`;
+};
+
+app.get("/test", (req, res) => {
+  res.json({ status: 200, message: "ok" });
+});
+
+app.get("/time", (req, res) => {
+  res.json({ status: 200, message: getDate() });
+});
+
+app.listen(port, () => console.log(`listening to port ${port}`));
